725610884a22902b3a5a46b850a7a071faa0d702
Junyu Zhu <alexzhuisme@gmail.com>
2022-02-25 00:50:53 -0800

    init
    init

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      <div>
+        <a href="https://github.com/alexzhuisme" target="_blank" rel="noreferrer">
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/junyuz/" target="_blank" rel="noreferrer">
+          <BsLinkedin />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default SocialMedia
